d2e548e2dcc84359257bf87aa66580d898106213
liang <xiaoleliang045@gmail.com>
2021-07-24 22:42:13 +0900

    config file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'), // __dirname is root directory, and path.resolve will concat __dirname and dist.
+        library: 'LXLUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+}
