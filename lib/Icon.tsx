e51205822f0f0c0372ff700a12393bedfbff83e8
liang <xiaoleliang045@gmail.com>
2021-07-26 22:40:25 +0900

    Icon component finished (basicly)

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './importIcons'
 import './icon.scss'
+import classes from './helpers/classes';
 
 // Props's Type
-interface IconProps {
-    name: string,
+interface IconProps extends React.SVGAttributes<SVGElement>{
+    name: string;
 }
 
 // declare Icon as a react function component
-const Icon:React.FunctionComponent<IconProps> = (props) => {
+const Icon: React.FunctionComponent<IconProps> = (props) => {
+
+    // es6 multiple object init
+    const {className, name, ...restProps} = props;
+
     return (
-        <svg className="lui-icon">
-            <use xlinkHref={`#${props.name}`} />
+        <svg className={classes('lui-icon', className)}
+            {...restProps}
+        >
+            <use xlinkHref={`#${name}`}/>
         </svg>
     )
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import Icon from './Icon'
 
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target);
+}
+
 ReactDOM.render(
     <div>
-        <Icon name="forward"/>
+        <Icon name="forward" 
+            onClick={fn}
+            onMouseEnter={fn}
+            onMouseLeave={fn}
+        />
     </div>
     , document.querySelector("#root"));
